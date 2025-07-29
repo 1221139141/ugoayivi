570d305169f90fead215cb3c562587fff4d12ac2
1221139141 <chenda.net.pro@gmail.com>
2025-07-29 23:32:55 +0200

    corrections bold style contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,22 +7,28 @@ export const metadata = {
 
 function page() {
   return (
-   <main className="mt-60">
+    <main className="mt-60">
       <div className="h-full flex justify-center bg-[#fefce8] border-3">
         <div className="w-2/3 p-20 text-justify">
           <div className="text-2xl mb-5">
             <h3 className="text-3xl font-[Geist] font-semibold mb-6 ">
-             Me contacter :
+              Me contacter :
             </h3>
           </div>
           <div className="text-2xl mb-4">
-            <h2 className="font-semibold">
-              Numéro de téléphone : +335 56 23 87 20
+            <h2>
+              <p className='font-semibold'>
+                Numéro de téléphone :
+              </p>
+              +335 56 23 87 20
             </h2>
-            <h2 className="font-semibold">
-              M'écrire à : ugo.ayivi@hedac.fr
+            <h2>
+              <p className='font-semibold'>
+                M'écrire à :
+              </p>
+              ugo.ayivi@hedac.fr
             </h2>
-            
+
           </div>
         </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ export default function Footer() {
       <ul className="space-y-1">
         <li className="text-zinc-300">2 Allée d&apos;Orléans</li>
         <li className="text-zinc-300">33000 BORDEAUX</li>
-        <li className="text-zinc-300">ugoayivi@gmail.com</li>
+        <li className="text-zinc-300">ugo.ayivi@hedac.fr</li>
         <li className="text-zinc-300">+335 56 23 87 20</li>
       </ul>
     </div>
