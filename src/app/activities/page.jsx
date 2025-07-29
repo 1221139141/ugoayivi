6814f816c8c73d1a863ca640eb815b418017d1ec
1221139141 <chenda.net.pro@gmail.com>
2025-07-29 22:56:13 +0200

    change frontend path acitvities

--- src/app/activities/page.jsx
+++ src/app/activities/page.jsx
@@ -3,37 +3,46 @@ import React from 'react'
 function page() {
   return (
     <main className="mt-60">
-      <h1 className='text-3xl'>Domaine d&apos;activités</h1>
-        <h2>
-          Droit social :
-        </h2>
-        <div className="text-2xl mb-4">
-          Maître AYIVI intervient aussi bien au niveau
-          conseil qu&apos;au niveau contentieux, aussi bien du côté salarié que
-          du côté employeur et vous accompagne tout au long de la
-          relation de travail (contrat de travail, exécution du contrat de
-          travail, rupture du contrat de travail, vie sociale, problématiques
-          RH et quotidienne, contentieux de la protection sociale, droit de
-          la sécurité sociale, mode amiable des règlement, transaction et
-          fiscalité de la transaction).
+      <div className="h-full flex justify-center bg-[#fefce8] border-3">
+        <div className="w-2/3 p-20 text-justify">
+          <div className="text-2xl mb-5">
+            <h3 className="text-3xl font-[Geist] font-semibold mb-6 ">
+              Domaines d’activité :
+            </h3>
+          </div>
+          <div className="text-2xl mb-4">
+            <h2>
+              Droit social:
+            </h2>
+            Maître AYIVI intervient aussi bien au niveau
+            conseil qu’au niveau contentieux, aussi bien du côté salarié que
+            du côté employeur et vous accompagne tout au long de la
+            relation de travail (contrat de travail, exécution du contrat de
+            travail, rupture du contrat de travail, vie sociale, problématiques
+            RH et quotidienne, contentieux de la protection sociale, droit de
+            la sécurité sociale, mode amiable des règlement, transaction et
+            fiscalité de la transaction).
+          </div>
+          <div className="text-2xl mb-4">
+            <h2>
+              Droit civil et commercial:
+            </h2>
+            Que ce soit devant les juridictions
+            civiles ou commerciales, Maître AYIVI usera de son haut niveau
+            d’expertise et de rigueur en la matière pour défendre vos
+            intérêts.
+          </div>
+          <div className="text-2xl mb-4">
+            <h2>
+              Droit de la famille :
+            </h2>
+            Que ce soit à l’amiable ou devant le juge
+            contentieux, Maître AYIVI vous accompagnera dans vos
+            démarches familiale (divorce, droit de garde, succession).
+          </div>
         </div>
-        <h2>
-          Droit civil et commercial :
-        </h2>
-        <div className="text-2xl mb-4">
-          Que ce soit devant les juridictions
-          civiles ou commerciales, Maître AYIVI usera de son haut niveau
-          d&apos;expertise et de rigueur en la matière pour défendre vos
-          intérêts.
-        </div>
-        <div className="text-2xl mb-4">
-          <h2>
-            Droit de la famille :
-          </h2>
-          Que ce soit à l&apos;amiable ou devant le juge
-          contentieux, Maître AYIVI vous accompagnera dans vos
-          démarches familiale (divorce, droit de garde, succession).
-        </div>
+      </div>
+
     </main>
   )
 }
