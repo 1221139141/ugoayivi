08ef8f7e1b42d4e2e43f76c6e71128a17f1439ab
1221139141 <chenda.net.pro@gmail.com>
2025-07-29 21:58:34 +0200

    change texts of introduction

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import maitre from "../../public/images/maitre-ayivi.jpg"
-export default function Accueil() {
+import Click from "../components/Click";
+
+export default function Accueil({ monTitre }) {
   return (
     <main className="mt-20 text-center">
       <div>
         <section className="relative border-2 border-transparent bg-[url('../../public/images/miroir-eau.jpg')] bg-cover bg-center h-200 saturate-200">
           <h2 className="bg-gray-50/50 py-2 w-1/2 text-3xl sm:text-4xl xl:text-5xl mt-110  text-gray-900 text-left font-bold md:w-1/2">Votre partenaire légal pour chaque étape de votre carrière.</h2>
           <h3 className="mt-10 text-2xl sm:text-3xl xl:text-4xl text-left text-gray-900 text-semibold">Offrez vous une vie plus juste avec un avocat qui met en avant l&apos;humain.</h3>
+          <Click monTitre="Il est où le plan numero 1 pacs" />
           <div className="relative grid h-150 grid-cols-2 content-end gap-4 mb-10">
           </div>
         </section>
@@ -15,17 +18,32 @@ export default function Accueil() {
       <div className="h-full flex justify-center bg-[#fefce8] border-3">
         <div className="w-2/3 p-20 text-justify">
           <div className="text-2xl mb-5">
-            <h3 className="text-3xl font-[Geist] font-semibold mb-6 ">Pour vous représenter</h3>
-            <p className="font-[Geist]"> d&apos;un xxx et xx obtenu à l&apos;Université xx, Maître Ugo AYIVI est inscrit au Barreau de xx.</p>
+            <h3 className="text-3xl font-[Geist] font-semibold mb-6 ">Maître AYIVI est titulaire d&apos;un Master II droit des affaires de
+              Aix-Marseille</h3>
+            <p className="font-[Geist]">Installé depuis plusieurs années à Bordeaux, les juridictions bordelaises et
+              girondines n&apos;ont plus aucun secret pour lui.</p>
           </div>
-          <div className="text-2xl mb-4 font-[Geist]">Après une première expérience en juridiction au Tribunal judiciaire de xx, il a débuté son activité professionnelle au sein d&apos;un cabinet pénaliste xx de renom, dont ceux de xx. Ces premières expériences lui ont permis d&apos;acquérir de solides compétences dans la gestion de problématiques juridiques complexes.</div>
+          <div className="text-2xl mb-4 font-[Geist]">Sa pratique, enrichie par ses différentes expériences professionnelles est
+            aujourd&apos;hui axée vers le droit social, le droit pénal et le contentieux civil et
+            commercial.</div>
+          <div className="text-2xl mb-4">Grâce à sa double expérience en entreprise et en cabinet d’avocat,
+            Maître AYVI est aujourd&apos;hui à même d&apos;anticiper et traiter aussi bien
+            les problématiques opérationnelles que juridiques et vous conseiller
+            au mieux dans vos démarches.</div>
           <div className="text-2xl mb-4">
-            Maître Ugo AYIVI intervient principalement en droit social, tant pour défendre les mis en cause à tous les stades de la procédure que pour assister les victimes en entreprises.
+            Maître AYIVI compte notamment parmi les quelques praticiens des
+            modes amiables de règlements à l&apos;échelle locale ce qui rend
+            particulièrement efficace dans la résolution négociée des différents.
           </div>
           <div className="text-2xl mb-4">
-          Passionné par la défense pénale et l&apos;art oratoire, il a été élu Premier Secrétaire de la Conférence du stage du barreau de Bordeaux pour l&apos;année 2023.
-          Rompu à l&apos;exercice de la plaidoirie et fort de son expérience, il saura vous accompagner avec pugnacité, efficacité, professionnalisme et humanité devant toutes les juridictions françaises.
-        </div>
+            Sa pratique est axée sur des valeurs des rigueurs, de réactivité et
+            de résultats.
+          </div>
+          <div className="text-2xl mb-4">
+            Que vous soyez salarié ou employeur, qu&apos;importe les enjeux, Maître
+            AYIVI emploiera toutes ses compétences afin de vous
+            accompagner tout le long de vos démarches juridiques.
+          </div>
         </div>
         <div className="">
           <Image
